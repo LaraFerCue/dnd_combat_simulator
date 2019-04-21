bf6452db9a2159006b344dd9b87b39089c6db359
lara <larafercue@gmail.com>
2019-04-21 16:57:10 +0200

    Using the damage system for the spells

--- dnd/models/spell.py
+++ dnd/models/spell.py
@@ -1,14 +1,12 @@
 from typing import List
 
+from dnd.models.damage import Damage, DamageType
 from dnd.models.die import Die
 
 
 class Spell:
     def __init__(self, damage: List[Die]):
-        self.__damage = damage
+        self.__damage = Damage(damage, DamageType.MAGIC)
 
     def get_damage(self) -> int:
-        damage = 0
-        for die in self.__damage:
-            damage += die.roll()
-        return damage
+        return self.__damage.get_damage()
