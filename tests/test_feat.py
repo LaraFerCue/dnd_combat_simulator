9151651b4d9804cff45f0dc97be1e1f7d7b06641
lara <larafercue@gmail.com>
2019-04-22 09:36:03 +0200

    Test resistance

--- tests/test_feat.py
+++ tests/test_feat.py
@@ -1,7 +1,14 @@
 from dnd.models.damage import DamageType
-from dnd.models.feat import Feat
+from dnd.models.feat import Feat, Resistance
 
 
 def test_get_feat_by_name():
     feat = Feat.get_feat_by_name(name='Resistance', damage_type=DamageType.PIERCING)
     assert feat.name == "Resistance"
+
+
+def test_resistance():
+    resistance = Resistance(DamageType.PIERCING)
+
+    assert resistance.modify_damage(10, DamageType.PIERCING) == 5
+    assert resistance.modify_damage(10, DamageType.MAGIC) == 10
