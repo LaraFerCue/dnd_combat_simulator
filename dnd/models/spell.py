5618cb779a0c91405738b92f41439777a9f1cb7b
lara <larafercue@gmail.com>
2019-04-25 12:26:38 +0200

    Compare spells

--- dnd/models/spell.py
+++ dnd/models/spell.py
@@ -7,3 +7,24 @@ class Spell:
 
     def get_damage(self) -> int:
         return self.__damage.get_damage()
+
+    def __hash__(self) -> int:
+        return hash(self.__damage)
+
+    def __eq__(self, other: 'Spell'):
+        return hash(self) == hash(other)
+
+    def __gt__(self, other):
+        return hash(self) > hash(other)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __ge__(self, other):
+        return self > other or self == other
+
+    def __lt__(self, other):
+        return not self >= other
+
+    def __le__(self, other):
+        return not self > other
--- tests/test_models/test_spell.py
+++ tests/test_models/test_spell.py
@@ -1,5 +1,5 @@
 from dnd.models.damage import DamageType, Damage
-from dnd.models.die import D10
+from dnd.models.die import D10, D12
 from dnd.models.spell import Spell
 
 
@@ -8,3 +8,16 @@ def test_spell():
 
     for _ in range(0, 5000):
         assert spell.get_damage() in range(1, 11)
+
+
+def test_compare_spells():
+    spell1 = Spell(Damage([D10], DamageType.MAGIC_ACID))
+    spell2 = Spell(Damage([D10], DamageType.MAGIC_ACID))
+    spell3 = Spell(Damage([D12], DamageType.MAGIC_ACID))
+    spell4 = Spell(Damage([D12], DamageType.MAGIC_COLD))
+    spell5 = Spell(Damage([D10], DamageType.MAGIC_COLD))
+
+    assert spell1 == spell2
+    assert spell1 != spell3
+    assert spell1 != spell4
+    assert spell1 != spell5
