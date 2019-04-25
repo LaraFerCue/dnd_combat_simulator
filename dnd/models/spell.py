f27eed61f15d7d706082bf1077cb73f981f5d705
lara <larafercue@gmail.com>
2019-04-25 14:50:11 +0200

    Adding high level spells and maximum casting times

--- dnd/models/spell.py
+++ dnd/models/spell.py
@@ -1,11 +1,29 @@
 from dnd.models.damage import Damage
 
 
+class SpellWornOut(BaseException):
+    pass
+
+
 class Spell:
-    def __init__(self, damage: Damage):
+    def __init__(self, damage: Damage, spell_lvl: int = 0):
         self.__damage = damage
+        self.__spell_lvl = spell_lvl
+        self.__used: int = 0
+        self.slots = 0
+
+    @property
+    def level(self) -> int:
+        return self.__spell_lvl
+
+    @property
+    def times_used(self) -> int:
+        return self.__used
 
     def get_damage(self) -> int:
+        if self.__used >= self.slots:
+            raise SpellWornOut(f"Spell casted maximum number of times ({self.slots})")
+        self.__used += 1
         return self.__damage.get_damage()
 
     def __hash__(self) -> int:
--- tests/test_models/test_spell.py
+++ tests/test_models/test_spell.py
@@ -1,10 +1,13 @@
+import pytest
+
 from dnd.models.damage import DamageType, Damage
 from dnd.models.die import D10, D12
-from dnd.models.spell import Spell
+from dnd.models.spell import Spell, SpellWornOut
 
 
 def test_spell():
     spell = Spell(Damage([D10], DamageType.MAGIC_ACID))
+    spell.slots = 1000000
 
     for _ in range(0, 5000):
         assert spell.get_damage() in range(1, 11)
@@ -21,3 +24,13 @@ def test_compare_spells():
     assert spell1 != spell3
     assert spell1 != spell4
     assert spell1 != spell5
+
+
+def test_high_level_spells():
+    spell1 = Spell(Damage([D10], DamageType.MAGIC_COLD), spell_lvl=1)
+    spell1.slots = 1
+
+    assert spell1.get_damage() in range(1, 11)
+    with pytest.raises(SpellWornOut):
+        spell1.get_damage()
+    assert spell1.times_used == 1
