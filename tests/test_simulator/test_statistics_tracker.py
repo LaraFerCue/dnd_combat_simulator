57563630d2de684566eece8ed170b7b97cf69535
lara <larafercue@gmail.com>
2019-04-26 17:28:13 +0200

    Moving statistics files to a new location

--- tests/test_simulator/test_statistics_tracker.py
+++ tests/test_simulator/test_statistics_tracker.py
@@ -7,9 +7,9 @@ from dnd.simulator.statistics_tracker import StatisticsTracker
 def test_statistics_tracker_one_track():
     statistics_tracker = StatisticsTracker()
 
-    with open('tests/resources/statistics_one_track.json') as json_file:
+    with open('tests/resources/statistics/statistics_one_track.json') as json_file:
         statistics_tracker.add(json.load(json_file), Combat.Result.WIN)
-    with open('tests/resources/statistics_one_track.csv') as csv_file:
+    with open('tests/resources/statistics/statistics_one_track.csv') as csv_file:
         csv_data = csv_file.read()
     assert statistics_tracker.to_csv() == csv_data
 
@@ -17,10 +17,10 @@ def test_statistics_tracker_one_track():
 def test_statistics_tracker_several_tracks():
     statistics_tracker = StatisticsTracker()
 
-    with open('tests/resources/statistics_several_tracks.json') as json_file:
+    with open('tests/resources/statistics/statistics_several_tracks.json') as json_file:
         for track in json.load(json_file):
             statistics_tracker.add(track, Combat.Result.WIN)
-    with open('tests/resources/statistics_several_tracks.csv') as csv_file:
+    with open('tests/resources/statistics/statistics_several_tracks.csv') as csv_file:
         csv_data = csv_file.read()
     assert statistics_tracker.to_csv() == csv_data
 
