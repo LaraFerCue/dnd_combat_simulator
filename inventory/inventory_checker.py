3b80bc7c6f03b0d7e3e7f9068b776b38640b79cf
lara <larafercue@gmail.com>
2019-04-26 10:33:51 +0200

    Add a python script to check the correctness of the inventory

---
+++ inventory/inventory_checker.py
@@ -0,0 +1,22 @@
+import logging
+from pathlib import Path
+
+from dnd.utils.file_utils import MalformedWeaponProperties, create_weapon_from_json_file, \
+    create_armor_from_json_file
+
+log = logging.getLogger('inventory_checker')
+log.setLevel(logging.DEBUG)
+
+try:
+    for weapon_json_file in Path('weapons').iterdir():
+        try:
+            create_weapon_from_json_file(weapon_json_file)
+        except MalformedWeaponProperties as exception:
+            log.exception(exception)
+    for armor_json_file in Path('armors').iterdir():
+        try:
+            create_armor_from_json_file(armor_json_file)
+        except MalformedWeaponProperties as exception:
+            log.exception(exception)
+except OSError as exception:
+    log.exception(exception)
